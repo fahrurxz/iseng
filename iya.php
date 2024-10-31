3c038d5d1bd86e6efeaca6356bebb1924342d816
Fahrur Rozi <142473573+fahrurxz@users.noreply.github.com>
2024-10-31 07:44:21 +0000

    x

---
+++ iya.php
@@ -0,0 +1,26 @@
+<?php
+# Fill our vars and run on cli
+# $ php -f db-connect-test.php
+
+$dbname = 'xule';
+$dbuser = 'xule';
+$dbpass = 'qwert1@345';
+$dbhost = '14.225.192.36';
+
+$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysql_select_db($dbname) or die("Could not open the db '$dbname'");
+
+$test_query = "SHOW TABLES FROM $dbname";
+$result = mysql_query($test_query);
+
+$tblCnt = 0;
+while($tbl = mysql_fetch_array($result)) {
+  $tblCnt++;
+  #echo $tbl[0]."<br />\n";
+}
+
+if (!$tblCnt) {
+  echo "There are no tables<br />\n";
+} else {
+  echo "There are $tblCnt tables<br />\n";
+}
